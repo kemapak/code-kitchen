9fc1fa7bd07c92a0629e19b877b48cc8fd3cfa8e
Kem Apak <kem@softwarenotes.net>
2022-11-26 02:42:37 -0600

    Local and session storage code example update, add CSS and JavaScript.

--- src/html-session-local-storage/main.js
+++ src/html-session-local-storage/main.js
@@ -1,4 +1,37 @@
 window.addEventListener('load', ()=> {
 
+    let localStorageKey = 'userToken';
+    let sessionStorageKey = 'customization'
+
+    let localStorageElement = document.getElementById('localStorageValue');
+    let sessionStorageElement = document.getElementById('sessionStorageValue');
+
+    localStorageElement.value = localStorage.getItem(localStorageKey);
+    sessionStorageElement.value = sessionStorage.getItem(sessionStorageKey);
+
+    document.getElementById('setLocalStorage').addEventListener('click', () => {
+        localStorage.setItem(localStorageKey, localStorageElement.value);
+    });
+
+    document.getElementById('clearLocalStorage').addEventListener('click', () => {
+        localStorage.removeItem(localStorageKey);
+        localStorageElement.value = localStorage.getItem(localStorageKey);
+    });
+
+    document.getElementById('setSessionStorage').addEventListener('click', () => {
+        sessionStorage.setItem(sessionStorageKey, sessionStorageElement.value);
+    });
+
+    document.getElementById('clearSessionStorage').addEventListener('click', () => {
+        sessionStorage.removeItem(sessionStorageKey);
+        sessionStorageElement.value = sessionStorage.getItem(sessionStorageKey);
+    });
+
+    document.getElementById('clearAllStorage').addEventListener('click', () => {
+        localStorage.clear();
+        localStorageElement.value = localStorage.getItem(localStorageKey);
+        sessionStorage.clear();
+        sessionStorageElement.value = sessionStorage.getItem(sessionStorageKey);
+    });
 });
 
